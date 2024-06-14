c4c4c3310d2b41eb9cc11249dc73121542c0ce4f
AluminumSoup <67250917+AluminumSoup@users.noreply.github.com>
2024-06-14 14:12:55 -0700

    Create random-color-tool.js

---
+++ random-color-tool.js
@@ -0,0 +1,10 @@
+//a random color 
+chooseColorBtn.addEventListener("click", chooseRandomColor());
+
+function chooseRandomColor(){
+  const redValue = Math.floor(Math.random() * 256);
+  const greenValue = Math.floor(Math.random() * 256);
+  const blueValue = Math.floor(Math.random() * 256);
+
+  document.getElementById("color-display").style.backgroundColor = "#CCCC00";
+};
