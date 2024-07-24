3da4001c7a1337674795f7f7dd938f67b27d85c8
AluminumSoup <67250917+AluminumSoup@users.noreply.github.com>
2024-07-23 18:12:19 -0700

    Update logic-grid-tool.js

--- logic-grid-tool.js
+++ logic-grid-tool.js
@@ -1,13 +1,14 @@
-document.getElementById("") += `<label for=""></label><br>
-      <input type="text" name="" id=""/>`
-
+/* document.getElementById("") += `<label for=""></label><br>
+      <input type="text" name="" id=""/>`*/
+const nameInputs = document.getElementById("name-inputs");
 let namingNum1 = document.getElementById("row-num").innerText; 
 let namingNum2 = document.getElementById("left-col-num").innerText; 
 let namingNum3 = document.getElementById("right-col-num").innerText; 
 
 
+nameInputs.innerHTML += `<label for="row-${j}">Row</label><br><input type="text" name="row-${j}" id="row-${j}"/><br>`;
+
+
 for (let i = 0; i < 3; i++;){
-  for (let j = 0; j < namingNum[i]; j++;){ <!-- Oh im so sure that 2nd j expression is not right -->
-    
-  }
+
 }
